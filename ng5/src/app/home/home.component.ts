2b80e83dfb5594e8ae7c235e9f0e5029eae85838
DESKTOP-Q15IRBB\Slavarenik <SlavarenikFlance@gmail.com>
2018-02-23 00:52:59 +0300

    Animation home.component.ts

--- ng5/src/app/home/home.component.ts
+++ ng5/src/app/home/home.component.ts
@@ -1,16 +1,37 @@
 import { Component, OnInit } from '@angular/core';
-
+import { trigger,style,transition,animate,keyframes,query,stagger } from '@angular/animations';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
+  animations: [
+
+    trigger('goals', [
+      transition('* => *', [
+
+        query(':enter', style({ opacity: 0 }), {optional: true}),
+
+        query(':enter', stagger('500ms', [
+          animate('.6s ease-in', keyframes([
+            style({opacity: 0, transform: 'translateY(-10%)', offset: 0}),
+            style({opacity: .5, transform: 'translateY(15px)',  offset: 0.3}),
+            style({opacity: 1, transform: 'translateY(0)',     offset: 1.0}),
+          ]))]), {optional: true})
+      ])
+    ])
+
+  ]
 })
 export class HomeComponent implements OnInit {
 
   itemCount: number;
   btnText: string = 'Add an item';
   goalText: string = 'My first goal';
-  goals = [];
+  goals = [
+    'My first life goal',
+    'I want to climb a mountain',
+    'Go ice skiing'
+  ];
 
   constructor() { }
 
